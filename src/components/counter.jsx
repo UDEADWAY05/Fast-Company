d8619a6062d0b94e6d49db26ac67cfb222db1196
Dmitry Kasperov <120218850+UDEADWAY05@users.noreply.github.com>
2023-03-14 22:40:48 +0300

    Update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,16 +3,14 @@ import API from "../API";
 
 const Users = () => {
     const [users, setUsers] = useState(API.users.fetchAll())
-    const hadleDelete = (useerId) => {
-        const delButton = (id) => {
-            console.log(id)
-            
-            setUsers((prevState) => prevState.filter(item=>item._id !== id))
-            console.log(users)
-            console.log(users.length)
-        }
-        return <>
-            <table className="table">
+    const handleDelete = (id) => {           
+        setUsers((prevState) => prevState.filter(item=>item._id !== id))
+    }
+    const renderPhrase = () => {
+        const textPhrase = users.length > 0 ? (users.length > 4 ? `${users.length} человек тусанёт с тобой сегодня` : (users.length === 1 ? `${users.length} человек тусанёт с тобой сегодня` : `${users.length} человека тусанёт с тобой сегодня`)) : "Никто с тобой не тусанёт"
+        const stylePhrase = `badge bg-${users.length > 0 ? "primary" : "danger"}`
+        const tableEl = () => {
+            return <> <table className="table">
             <thead>
             <tr>
                 <th scope="col">Имя</th>
@@ -24,39 +22,32 @@ const Users = () => {
             </tr>
             </thead>
             <tbody>
-                    {users.map((user, idx) => 
-                    <tr id={idx}>
+                    {users.map((user) => 
+                    <tr id={user._id}>
                         <td key="name">{user.name}</td>
-                            <td key="qualities">
-                                <table>
-                                    <tbody>
-                                        <tr>
-                                        {user.qualities.map((qal) => {
-                                        return <td className={'badge m-1 bg-'+ qal.color} >{qal.name}</td>    
-                                        })}</tr>
-                                    </tbody>
-                                </table>
+                            <td key="qualities">                                        
+                                {user.qualities.map((qal) => {
+                                return <span className={'badge m-1 bg-'+ qal.color} >{qal.name}</span>    
+                                })}
                             </td>
                         <td key="profession">{user.profession.name}</td>
                         <td key="exp">{user.completedMeetings}</td>
-                            <td key="bal">{`${user.rate}` + '/5'}</td>
-                            <td><button className="btn btn-danger" type="submit" onClick={() => delButton(user._id)}>delete</button></td>    
+                            <td key="bal">{user.rate + '/5'}</td>
+                            <td><button className="btn btn-danger" type="submit" onClick={() => handleDelete(user._id)}>delete</button></td>    
                     </tr>
                     
                 )}                     
             </tbody>
-            </table>
+            </table></>
+        }
+        return <>
+            <h2><span className={stylePhrase}>{textPhrase}</span></h2>
+            {users.length === 0? "" : tableEl()}
         </>
-    }
-    const renderPhrase = () => {
-        const textPhrase = users.length > 0 ? (users.length > 4 ? `${users.length} человек тусанёт с тобой сегодня` : (users.length === 1 ? `${users.length} человек тусанёт с тобой сегодня` : `${users.length} человека тусанёт с тобой сегодня`)) : "Никто с тобой не тусанёт"
-        const stylePhrase = "badge bg-" + `${users.length > 0 ? "primary": "danger" }`
-        return <h2><span className={stylePhrase}>{textPhrase}</span></h2>
     }
 
     return <>
         {renderPhrase()}
-        {hadleDelete()}
     </>
 }
 
